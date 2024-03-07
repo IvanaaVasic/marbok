e09316386780cd131539573c8e317c84c7b55943
IvanaaVasic <ivana.p.vasic@gmail.com>
2024-03-07 01:05:44 +0100

    feat: footer responsive

--- app/page.js
+++ app/page.js
@@ -80,7 +80,9 @@ export default async function Home() {
       </div>
       <div className={styles.footer}>
         <div className={styles.footerInfo}>
-          <p>marbok.bgd@gmail.com</p> | <p>011/ 3472 890</p> |
+          <p>marbok.bgd@gmail.com</p>{" "}
+          <span className={styles.separator}>|</span> <p>011/ 3472 890</p>{" "}
+          <span className={styles.separator}>|</span>
           <p>067/ 77 00 771</p>
         </div>
         <p>Copyright © {new Date().getFullYear()}, Marbok DOO</p>
