3f222dad139f05e1e354d3965f98af9c886a6ab8
IvanaaVasic <ivana.p.vasic@gmail.com>
2024-03-29 13:55:09 +0100

    feat: navigation big sreens styling

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,18 +1,42 @@
 import React from "react";
 import styles from "./Navigation.module.css";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import clsx from "clsx";
 
 function Navigation() {
+  const router = useRouter();
+
+  const pathName = router.pathname;
+
   return (
     <div className={styles.container}>
       <Link href={`/`} className={styles.listItem}>
-        <p className={styles.link}>Naslovna</p>
+        <p
+          className={`${styles.link} ${
+            pathName === "/" ? styles.activeLink : ""
+          }`}
+        >
+          Naslovna
+        </p>
       </Link>
       <Link href={`/catalog`} className={styles.listItem}>
-        <p className={styles.link}>Katalog proizvoda</p>
+        <p
+          className={`${styles.link} ${
+            pathName === "/catalog" ? styles.activeLink : ""
+          }`}
+        >
+          Katalog proizvoda
+        </p>
       </Link>
       <Link href={`/contact`} className={styles.listItem}>
-        <p className={styles.link}>Kontakt</p>
+        <p
+          className={`${styles.link} ${
+            pathName === "/contact" ? styles.activeLink : ""
+          }`}
+        >
+          Kontakt
+        </p>
       </Link>
     </div>
   );
