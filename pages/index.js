79ffe05ad45f3f15806d6798be9ed8fb173ffe66
marbokbgd-cmyk <marbok.bgd@gmail.com>
2026-09-23 10:39:22 +0200

    Redirect homepage to B2B catalog

--- pages/index.js
+++ pages/index.js
@@ -1,77 +1,14 @@
-import styles from "./Home.module.css";
-import Layout from "@/components/Layout/Layout";
-import {
-    getPages,
-    getImages,
-    getHeading,
-    getBrandImages,
-    getCategories,
-    getStores,
-} from "@/sanity/sanity-utils";
-import {
-    usePages,
-    useHeroImages,
-    useHeading,
-    useBrandImages,
-    useCategories,
-} from "@/hooks/usePages";
-import ImagesSlider from "@/components/ImagesSlider/ImagesSlider";
-import Heading from "@/components/Heading/Heading";
-import ProductsView from "@/components/ProductsView/ProductsView";
-import Experience from "@/components/Experience/Experience";
-import BrandSlider from "@/components/BrandSlider/BrandSlider";
-
-function Home({
-    initialPages,
-    initialHeroImages,
-    initialHeading,
-    initialBrandImages,
-    initialCategory,
-    initialStores,
-}) {
-    const pages = usePages() || initialPages;
-    const heroImages = useHeroImages() || initialHeroImages;
-    const heading = useHeading() || initialHeading;
-    const brandImages = useBrandImages() || initialBrandImages;
-    const categories = useCategories() || initialCategory;
-
-    const newproducts = pages.content.find(
-        (obj) => obj.title === "Novi proizvodi"
-    );
-
-    return (
-        <Layout pages={pages} categories={categories} stores={initialStores}>
-            {(filteredProducts) => (
-                <div className={styles.container}>
-                    <ImagesSlider images={heroImages} />
-                    <Heading mainHeading={heading?.[0]} />
-                    <ProductsView products={newproducts} />
-                    <Experience />
-                    <BrandSlider brandImages={brandImages} />
-                </div>
-            )}
-        </Layout>
-    );
+function Home() {
+    return null;
 }
 
 export default Home;
 
-export async function getServerSideProps() {
-    const initialPages = await getPages();
-    const initialHeroImages = await getImages();
-    const initialHeading = await getHeading();
-    const initialBrandImages = await getBrandImages();
-    const initialCategory = await getCategories();
-    const initialStores = await getStores();
-
+export function getServerSideProps() {
     return {
-        props: {
-            initialPages,
-            initialHeroImages,
-            initialHeading,
-            initialBrandImages,
-            initialCategory,
-            initialStores,
+        redirect: {
+            destination: "/catalog",
+            permanent: false,
         },
     };
 }
